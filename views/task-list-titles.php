fa6f908b24ee3d02a9fe46bf7ba10919174b76ba
Halyna Urbanska <webenot@mail.ua>
2020-07-20 08:40:37 +0300

    Fix bugs with pagination

--- views/task-list-titles.php
+++ views/task-list-titles.php
@@ -4,20 +4,20 @@
 <thead>
 <tr>
   <th scope="col">
-    <a href="<?php $titleParams['orderBy'] = 'name'; $titleParams['order'] = $order === 'ASC' && $orderBy === 'name' ? 'DESC' : 'ASC'; echo $baseUrl . '?' . makeParamsString($params); ?>">
+    <a href="<?php $titleParams['orderBy'] = 'name'; $titleParams['order'] = $order === 'ASC' && $orderBy === 'name' ? 'DESC' : 'ASC'; echo $baseUrl . '?' . makeParamsString($titleParams); ?>">
       <span class="sort-title">Name</span>
       <span class="sort-order"><?php echo $order === 'ASC' && $orderBy === 'name' ? '▲' : '▼'; ?></span>
     </a>
   </th>
   <th scope="col">
-    <a href="<?php $titleParams['orderBy'] = 'email'; $titleParams['order'] = $order === 'ASC' && $orderBy === 'email' ? 'DESC' : 'ASC'; echo $baseUrl . '?' . makeParamsString($params); ?>">
+    <a href="<?php $titleParams['orderBy'] = 'email'; $titleParams['order'] = $order === 'ASC' && $orderBy === 'email' ? 'DESC' : 'ASC'; echo $baseUrl . '?' . makeParamsString($titleParams); ?>">
       <span class="sort-title">Email</span>
       <span class="sort-order"><?php echo $order === 'ASC' && $orderBy === 'email' ? '▲' : '▼'; ?></span>
     </a>
   </th>
   <th scope="col">Task</th>
   <th scope="col">
-    <a href="<?php $titleParams['orderBy'] = 'status'; $titleParams['order'] = $order === 'ASC' && $orderBy === 'status' ? 'DESC' : 'ASC'; echo $baseUrl . '?' . makeParamsString($params); ?>">
+    <a href="<?php $titleParams['orderBy'] = 'status'; $titleParams['order'] = $order === 'ASC' && $orderBy === 'status' ? 'DESC' : 'ASC'; echo $baseUrl . '?' . makeParamsString($titleParams); ?>">
       <span class="sort-title">Status</span>
       <span class="sort-order"><?php echo $order === 'ASC' && $orderBy === 'status' ? '▲' : '▼'; ?></span>
     </a>
